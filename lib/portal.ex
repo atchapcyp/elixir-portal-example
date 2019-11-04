7b7b8955f2093d4478d2b3a69bc4f37114301d1b
SHA <atcha.crypto@gmail.com>
2019-11-04 22:21:03 +0700

    Add module portal

--- lib/portal.ex
+++ lib/portal.ex
@@ -15,4 +15,29 @@ defmodule Portal do
   def hello do
     :world
   end
+
+  @doc """
+  Starts transfering `data` from `left` to `right`.
+  """
+  defstruct [:left, :right]
+
+  def transfer(left, right, data) do
+    for item <- data do
+      Portal.Door.push(left, item)
+    end
+
+    %Portal{left: left, right: right}
+  end
+
+  @doc """
+  Pushes data to the right in the given `portal`.
+  """
+  def push_right(portal) do
+    case Portal.Door.pop(portal.left) do
+      :error -> :ok
+      {:ok, h} -> Portal.Door.push(portal.right, h)
+    end
+
+    portal
+  end
 end
